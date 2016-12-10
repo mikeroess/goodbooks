6ecd70463092c0cf4f1a56d7cd0d44e8ba9a2db3
mwr <michael.roess@gmail.com>
2016-12-10 15:45:00 -0500

    Interpolate shelf length

--- frontend/components/shelf/shelf_index_item.jsx
+++ frontend/components/shelf/shelf_index_item.jsx
@@ -5,10 +5,11 @@ class ShelfIndexItem extends React.Component {
 
 render () {
   const linkPath = `/user/shelf/${this.props.shelf.shelfId}`;
+
   return (
       <li>
       <Link to={ linkPath } className="shelfIndexLink">
-        {this.props.shelf.title} {this.props.shelf.count}
+        {this.props.shelf.title} {'('}{this.props.shelf.count}{')'}
       </Link>
       </li>
   );
