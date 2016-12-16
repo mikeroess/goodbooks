daaee9720bd4d8fab9750bea529648f44898fe79
mwr <michael.roess@gmail.com>
2016-12-16 09:26:05 -0500

    Permit logout from bookDetail view

--- frontend/components/book/book_detail_container.jsx
+++ frontend/components/book/book_detail_container.jsx
@@ -3,15 +3,19 @@ import BookDetail from './book_detail';
 import { fetchBook } from '../../actions/book_actions';
 
 const mapStateToProps = ( { shelves, session, review, books, readStatuses }) => {
-  return {
-    bookDetails: books.bookDetails,
-    reader: books.readers,
-    booksShelves: books.bookDetails.shelves,
-    userShelves: shelves.shelves,
-    review: review.review,
-    currentUsername: session.currentUser.username,
-    readStatuses: readStatuses.readStatuses
-  };
+  if (session.currentUser) {
+    return {
+      bookDetails: books.bookDetails,
+      reader: books.readers,
+      booksShelves: books.bookDetails.shelves,
+      userShelves: shelves.shelves,
+      review: review.review,
+      currentUsername: session.currentUser.username,
+      readStatuses: readStatuses.readStatuses
+    };
+  } else {
+    return {};
+  }
 };
 
 const mapDispatchToProps = (dispatch) => {
